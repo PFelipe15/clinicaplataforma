411821064f1886bad85130c7d2882a5b8cb80b4f
PFelipe15 <paulofelipet15@gmail.com>
2024-05-17 22:47:45 -0300

    Voltando para Home ao clicar no botão nos exames

--- src/app/exames-tratamentos/layout.tsx
+++ src/app/exames-tratamentos/layout.tsx
@@ -1,20 +1,18 @@
 import { NavigationUrls } from "@/components/layout/navigationurls";
 import { Button } from "@/components/ui/button";
-
+import Link from "next/link";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-       
-    
-          <main className="flex-grow flex container flex-col items-center  animate-fadeIn ">
-            <NavigationUrls/>
-            {children}
-            <Button className="m-4 self-end">Voltar</Button>
-          </main>
-   
-     
+    <main className="flex-grow flex container flex-col items-center  animate-fadeIn ">
+      <NavigationUrls />
+      {children}
+      <Link href="/">
+        <Button className="m-4 self-end">Voltar</Button>
+      </Link>
+    </main>
   );
 }
