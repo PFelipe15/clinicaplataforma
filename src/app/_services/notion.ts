afb69101bf77e3525c174caaf7bc21e4078ab8b5
PFelipe15 <paulofelipet15@gmail.com>
2024-05-23 22:35:00 -0300

    Adicionado verificao para propiedades do notion que estão a vir undefined, tratamento para blog

--- src/app/_services/notion.ts
+++ src/app/_services/notion.ts
@@ -2,9 +2,9 @@ import { unstable_noStore as noStore } from 'next/cache';
 import { Client } from '@notionhq/client'
 import { NotionDatabaseResponse } from '../_types/notion';
 import { NotionToMarkdown } from 'notion-to-md';
+import ImageNotFound from '../assets/Funcionarios/icon-image-not-found-free-vector.jpg'
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const DATABASE_ID = process.env.DATABASE_ID!;
-
  
 export async function getPosts(){
   noStore();
@@ -14,18 +14,17 @@ export async function getPosts(){
     });
 
      const typedResponse = ( response as  unknown) as NotionDatabaseResponse
- 
-    
+
  
      return typedResponse.results.map((post) => ({
-      id: post.id,
-      title: post.properties.titulo.title[0].plain_text,
-      slug: post.properties.slug.rich_text[0].plain_text,
-      tags:post.properties.Tags.multi_select.map((tag)=>tag.name),
-      descricao:post.properties.descricao.rich_text[0].plain_text,
-      imageCapa:post.properties.imageCapa.files[0].file.url,
-      createAt:post.created_time.toString
-     }));
+      id: post.id ?? "Desenvolvendo",
+      title: post.properties.titulo?.title[0]?.plain_text ?? "Em construção",
+      slug: post.properties.slug?.rich_text[0]?.plain_text ?? undefined,
+      tags: post.properties.Tags?.multi_select?.map((tag) => tag.name) ?? [],
+      descricao: post.properties.descricao?.rich_text[0]?.plain_text ?? "Essa postagem está em desenvolvimento",
+      imageCapa: post.properties.imageCapa?.files[0]?.file?.url ?? ImageNotFound,
+      createAt: post.created_time?.toString() ?? "Data de criação não disponível"
+    }));
 
 
 }
