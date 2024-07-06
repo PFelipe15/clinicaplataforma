08f0101a5610578a8756ef68ed3f0c8187c8ea8a
PFelipe15 <paulofelipet15@gmail.com>
2024-07-06 00:53:07 -0300

    image

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Logo from '../../app/assets/Logo.jpg';
+import Logo from '../../app/assets/logo_page-0001.jpg';
 import { MapPin, Mail } from "lucide-react";
 import { CgInstagram } from "react-icons/cg";
 import { BsWhatsapp } from "react-icons/bs";
@@ -11,9 +11,8 @@ const Footer = () => {
         <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start">
           <div className="flex flex-col items-center lg:items-start mb-8 lg:mb-0">
             <div className="flex items-center mb-4">
-              <Image src={Logo} alt="Logo" width={100} height={100} className="rounded-full shadow-lg" />
-              <h1 className="text-3xl font-extrabold ml-4">Urovida</h1>
-            </div>
+              <Image src={Logo} alt="Logo" width={200} height={200} className=" shadow-lg" />
+             </div>
             <nav className="flex flex-wrap justify-center lg:justify-start text-lg">
               <a href="/" className="mx-2 hover:scale-105 transition-all hover:text-gray-300">Home</a>
               <a href="/clinica" className="mx-2 hover:scale-105 transition-all hover:text-gray-300">A Clínica</a>
